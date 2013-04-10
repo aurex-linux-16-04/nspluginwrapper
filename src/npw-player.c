b679d611469abd213a78254ed03a2028dca2be2d
Daniel T Chen <crimsun@ubuntu.com>
2013-04-09 23:47:10 -0400

    Imported Debian patch 1.4.4-0ubuntu5

--- src/npw-player.c
+++ src/npw-player.c
@@ -24,8 +24,6 @@
 #include <dlfcn.h>
 #include <unistd.h>
 #include <glib.h>
-#include <glib/gstdio.h>
-#include <glib/gthread.h>
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
 #include <gdk/gdkkeysyms.h>
